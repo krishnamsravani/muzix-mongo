42c4fc842ac0fec6f6ed23f0ea2568a779930b7b
root <sravanikrishnam369@gmail.com>
2019-05-28 12:38:07 +0530

    initial commit

---
+++ src/main/java/com/stackroute/muzix/repository/TrackRepository.java
@@ -0,0 +1,9 @@
+package com.stackroute.muzix.repository;
+
+import com.stackroute.muzix.domain.Track;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TrackRepository extends MongoRepository<Track,Integer> {
+
+}
